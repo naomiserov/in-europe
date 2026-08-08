c1e796e8f68da3113545631bf36e745a8a306ef9
naomiserov <163772472+naomiserov@users.noreply.github.com>
2026-08-08 15:42:08 +0200

    Update firebase.js

--- cultural capital/firebase.js
+++ cultural capital/firebase.js
@@ -18,7 +18,32 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Export services
-export const db = getFirestore(app);
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import { getStorage } from
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
+
+import { getDatabase } from
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
+
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "cultural-capital-25cee.firebaseapp.com",
+  databaseURL: "https://console.firebase.google.com/project/cultural-capital-25cee/database/cultural-capital-25cee-default-rtdb/data/~2F",
+  projectId: "cultural-capital-25cee",
+  storageBucket: "cultural-capital-25cee.firebasestorage.app",
+  messagingSenderId: "430405827158",
+  appId: "1:430405827158:web:e60ae24126d2df462219f7",
+  measurementId: "G-62DJMCDQ7K"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app);
+
 export const storage = getStorage(app);
 
 
